e6cc2df8563db59ad3031cd12a96baa685d40bed
David John <davidspc@MacBook-Air.local>
2020-08-21 15:45:08 +0100

    duedate function added

--- src/components/to-do-list.jsx
+++ src/components/to-do-list.jsx
@@ -2,36 +2,55 @@ import React from "react";
 
 class ToDoList extends React.Component {
   state = {
-    list: ["Clean house", "Wash dishes", "Go shopping"],
+    list: [
+      { task: "Clean house", DateDue: "01 / 09 / 2020" },
+      { task: "Wash dishes", DateDue: "23 / 09 / 2020" },
+      { task: "Go shopping", DateDue: "16 / 09 / 2020" },
+    ],
     invalid: false,
-    newItem: "",
+    task: "",
+    dateDue: "",
   };
+
+  componentDidMount() {
+    this.handleSavedList();
+  }
 
   handleSubmit = (submitEvent) => {
     submitEvent.preventDefault();
-    console.log(this.state.invalid);
     if (this.state.invalid) {
-      const newItem = this.state.newItem;
-      const restOfWord = newItem.slice(1);
-      const firstLetter = newItem[0].toUpperCase();
+      const task = this.state.task;
+      const date = this.state.dateDue.split("-").reverse();
+      const formattedDate = date.map((num, index) => {
+        if (index < 2) {
+          return `${num} / `;
+        } else {
+          return ` ${num}`;
+        }
+      });
+      const restOfWord = task.slice(1);
+      const firstLetter = task[0].toUpperCase();
       const capitalisedWord = firstLetter + restOfWord;
 
       this.setState((currentState) => {
         return {
           invalid: !currentState.invalid,
-          list: [capitalisedWord, ...currentState.list],
-          newItem: "",
+          list: [
+            { task: capitalisedWord, DateDue: formattedDate.join("") },
+            ...currentState.list,
+          ],
+          task: "",
+          dateDue: "",
         };
       });
     }
   };
   handleInput = (inputEvent) => {
     const input = inputEvent.target.value;
-    const containsSpecialChar = /[!@#$%^&*£)(+=._-]/.test(input);
+    const containsSpecialChar = /[!@#$%^&*£)(+=._]/.test(input);
     if (!containsSpecialChar) {
       this.setState((currentState) => {
-        console.log(currentState);
-        return { newItem: input, invalid: true };
+        return { task: input, invalid: true };
       });
     }
   };
@@ -39,37 +58,97 @@ class ToDoList extends React.Component {
   handleDelete = (clickEvent) => {
     const itemName = clickEvent.target.id;
     const listArray = this.state.list;
+
     const ArrayWithRemovedItem = listArray.filter((item) => {
-      return item !== itemName;
+      return item.task !== itemName;
     });
     this.setState(() => {
       return { list: ArrayWithRemovedItem };
     });
   };
+
+  handleSave = (clickEvent) => {
+    localStorage.setItem("list", JSON.stringify(this.state.list));
+  };
+
+  handleSavedList = () => {
+    const list = JSON.parse(localStorage.getItem("list"));
+    this.setState(() => {
+      if (!list) {
+        return { list: this.state.list };
+      } else {
+        return { list: list };
+      }
+    });
+  };
+
+  handleClear = () => {
+    this.setState(() => {
+      localStorage.clear();
+      return {
+        list: [
+          {
+            task: "Add your tasks in the input form above!",
+            DateDue: "DateDue: 00/00/0000",
+          },
+        ],
+      };
+    });
+  };
+
+  handleDate = (inputEvent) => {
+    const input = inputEvent.target.value;
+    this.setState((currentState) => {
+      return { dateDue: input };
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>CheckList</h2>
+        <img
+          src="https://cdn.windowsreport.com/wp-content/uploads/2019/07/notepad-modern-design-concept.jpg"
+          alt=""
+        />
+        <div id="saveButton">
+          <p>
+            <button className="button" onClick={this.handleSave}>
+              Save List
+            </button>
+            |
+            <button className="button" onClick={this.handleClear}>
+              Clear List
+            </button>
+          </p>
+        </div>
         <form onSubmit={this.handleSubmit} className="form">
           <label>
-            Add to checklist:
+            Add to checklist with optional due date:
             <input
               type="text"
               onChange={this.handleInput}
-              value={this.state.newItem}
+              value={this.state.task}
+              className="input"
             />
           </label>
+          <input
+            type="date"
+            onChange={this.handleDate}
+            value={this.state.dateDue}
+            className="input"
+          />
           <button>Submit</button>
         </form>
         <ul id="list">
           {this.state.list.map((item, index) => {
             return (
-              <li key={item}>
+              <li key={item.task}>
                 <p>
                   {`
-                  ${item} | DateDue: 23/09/2020  |`}
+                  ${item.task} | ${item.DateDue} |`}
                   <button
-                    id={item}
+                    id={item.task}
                     className="listButton"
                     onClick={this.handleDelete}
                   >
